c8911be4102914a1430f83c2bc8a9a437b289f39
Marcus Dillavou <line72@line72.net>
2023-08-26 10:08:56 -0500

    Build Android 1.2.0-1

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CONFIG = transmogrifier.Config(
         app_store_url = 'https://apps.apple.com/us/app/go-montgomery-al/id1494071249'
     ),
     android_config = transmogrifier.MontclairAndroidConfig(
-        version = '1.0.2',
+        version = '1.2.0',
         revision = 1,
         app_id = 'com.gotransitapp.montgomeryal',
         play_store_url = 'https://play.google.com/store/apps/details?id=com.gotransitapp.montgomeryal'
